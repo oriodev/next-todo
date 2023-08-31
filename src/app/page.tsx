ebd6ccc20807b1043a82670593285f3f60c84b9d
ori <93719767+oriodev@users.noreply.github.com>
2023-08-31 18:20:31 +0100

    setup layout and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
-  return (
-    <h1>hello</h1>
-  )
+import prisma from "@/db"
+import Link from "next/link"
+
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
+
+  return ( <>
+
+    <header className='flex flex-row gap-5 items-center mb-4'>
+      <h1 className='text-3xl'>todos</h1>
+      <Link className='text-3xl hover:font-bold focus-within:font-bold' href='/new'>+</Link>
+    </header>
+      
+    <ul className='pl-4'>
+
+    </ul>
+
+  </> )
 }
